8aca0def4e6f0d07741f17b25862929c1d3651f9
osoucy <olivier@newton.co>
2023-07-06 21:48:28 -0400

    Columns and tables.

--- medaillon/models/dummy.py
+++ medaillon/models/dummy.py
@@ -1,5 +1,8 @@
 from typing import Literal
 
+from pydantic import Field
+from pydantic import ConfigDict
+
 from medaillon.contants import SUPPORTED_TYPES
 from medaillon.models.base import BaseModel
 
@@ -13,6 +16,9 @@ class Column(BaseModel):
     udf_name: str = None
     input_cols: list[str] = []
     udf_kwargs: dict = {}
+    test: int = Field(...)   # Set as required field, but put at end of list of fields
+
+    model_config = ConfigDict(extra='forbid')
 
 
 if __name__ == "__main__":
