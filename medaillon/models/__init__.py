233c448edf46e2c2eb5256459065f5d0795b19b7
osoucy <olivier@newton.co>
2023-07-07 02:45:48 -0400

    Update

--- medaillon/models/__init__.py
+++ medaillon/models/__init__.py
@@ -1,2 +1,3 @@
 from .column import Column
 from .table import Table
+from .database import Database
---
+++ tests/test_database.py
@@ -0,0 +1,50 @@
+from medaillon.models import Table
+from medaillon.models import Database
+from medaillon.models import Column
+
+
+def test_model():
+    db = Database(
+        name="flights",
+        tables=[
+            Table(
+                name="f1549",
+                columns=[
+                    {
+                        "name": "airspeed",
+                        "type": "double",
+                    },
+                    {
+                        "name": "altitude",
+                        "type": "double",
+                    },
+                ],
+                zone="SILVER",
+                parent_id="lakehouse.flights",
+            ),
+            Table(
+                name="f0002",
+                columns=[
+                    {
+                        "name": "airspeed",
+                        "type": "double",
+                    },
+                    {
+                        "name": "altitude",
+                        "type": "double",
+                    },
+                ],
+                zone="SILVER",
+                parent_id="lakehouse.flights",
+            ),
+        ],
+    )
+
+    print(db.tables[0].columns[0].name)
+
+    assert db.tables[0].columns[0].name == "airspeed"
+    assert type(db.tables[0].columns[0]) == Column
+
+
+if __name__ == "__main__":
+    test_model()
