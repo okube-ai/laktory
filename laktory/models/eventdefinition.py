4be5677d51bb6fc945d83ea723d4a3912e6bf342
osoucy <osoucy.transactions@gmail.com>
2023-07-15 03:22:01 -0400

    Update

--- laktory/models/eventdefinition.py
+++ laktory/models/eventdefinition.py
@@ -1,10 +1,26 @@
+import os
+
 from laktory.models.base import BaseModel
 from laktory.models.producer import Producer
-from laktory.models.ingestion_pattern import IngestionPattern
+from laktory.models.ingestionpattern import IngestionPattern
 
+
+# --------------------------------------------------------------------------- #
+# Main Class                                                                  #
+# --------------------------------------------------------------------------- #
 
 class EventDefinition(BaseModel):
     name: str
     description: str = None
     producer: Producer = None
     ingestion_pattern: IngestionPattern = IngestionPattern()
+    tstamp_col: str = "created"
+    landing_mount_path: str = os.getenv("LANDING_MOUNT_PATH", "mnt/landing/")  # TODO: Replace with setting
+
+    # ----------------------------------------------------------------------- #
+    # Paths                                                                   #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def landing_dirpath(self):
+        return f"{self.landing_mount_path}/events/{self.producer.name}/{self.name}/"
