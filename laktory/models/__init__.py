b42402eab70a3744cb26494c250c6e915f6c9aae
osoucy <osoucy.transactions@gmail.com>
2023-07-15 03:36:44 -0400

    Update

--- laktory/models/__init__.py
+++ laktory/models/__init__.py
@@ -3,6 +3,6 @@ from .column import Column
 from .database import Database
 from .eventdata import EventData
 from .eventdefinition import EventDefinition
-from .ingestion_pattern import IngestionPattern
+from .ingestionpattern import IngestionPattern
 from .producer import Producer
 from .table import Table
