48bccad7b72166859232db29958620f02ec0a981
osoucy <osoucy.transactions@gmail.com>
2023-07-23 00:22:13 -0400

    Update

--- tests/test_eventdata.py
+++ tests/test_eventdata.py
@@ -19,8 +19,8 @@ def test_eventdata():
     assert event.producer.name == "FDR"
     assert event.data["altitude"] == 20000
     assert event.data["_producer_name"] == "FDR"
-    assert event.data["_created_at"] == datetime(2023, 7, 1, 1, 0, 0, tzinfo=ZoneInfo("utc"))
-    assert event.created_at == datetime(2023, 7, 1, 1, 0, 0, tzinfo=ZoneInfo("utc"))
+    assert event.data["_created_at"] == datetime(2023, 7, 1, 1, 0, 0, tzinfo=ZoneInfo("UTC"))
+    assert event.created_at == datetime(2023, 7, 1, 1, 0, 0, tzinfo=ZoneInfo("UTC"))
     assert event.get_landing_filepath() == "mnt/landing/events/FDR/flight_record/2023/07/01/flight_record_20230701T010000000Z.json"
 
 
