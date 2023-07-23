de5912e1065d28003f19bfe6fa519ede00cdf942
osoucy <osoucy.transactions@gmail.com>
2023-07-23 01:46:02 -0400

    Update

--- laktory/readers/base.py
+++ laktory/readers/base.py
@@ -11,3 +11,17 @@ class BaseReader:
     @abstractmethod
     def read(self) -> DataFrame:
         pass
+        # return (
+        #     spark
+        #     .read
+        #     .format("json")
+        #     .option("multiLine", False)
+        #     # .option("mergeSchema", True)
+        #     # .option("cloudFiles.format", ip.fmt)
+        #     # .option("cloudFiles.schemaLocation", data_path)
+        #     # .option("cloudFiles.inferColumnTypes", True)
+        #     # .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
+        #     # .option("cloudFiles.allowOverwrites", True)
+        #     # .option("recursiveFileLookup", True)
+        #     .load(filepath)
+        # )
