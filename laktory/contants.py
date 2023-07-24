cf45553dc81c7d2325d7192356f62b168eabcbc3
osoucy <osoucy.transactions@gmail.com>
2023-07-24 02:49:40 -0400

    Update

--- laktory/contants.py
+++ laktory/contants.py
@@ -3,15 +3,15 @@ from pyspark.sql import types
 
 
 SUPPORTED_TYPES = {}
-for k in vars(types):
-    o = getattr(types, k)
-    if "Type" in k and inspect.isclass(o):
-        try:
-            typ = o()
-        except TypeError:
-            continue
-
-        if not hasattr(typ, "simpleString"):
-            continue
-
-        SUPPORTED_TYPES[typ.simpleString()] = typ
+# for k in vars(types):
+#     o = getattr(types, k)
+#     if "Type" in k and inspect.isclass(o):
+#         try:
+#             typ = o()
+#         except TypeError:
+#             continue
+#
+#         if not hasattr(typ, "simpleString"):
+#             continue
+#
+#         SUPPORTED_TYPES[typ.simpleString()] = typ
