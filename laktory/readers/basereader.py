b2b93b029ea55dada37e43253eb35ad7f0dfe759
osoucy <osoucy.transactions@gmail.com>
2023-07-25 02:05:50 -0400

    Update

--- laktory/readers/basereader.py
+++ laktory/readers/basereader.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from abc import abstractmethod
 
 from pyspark.sql.dataframe import DataFrame
@@ -6,7 +7,16 @@ from laktory.models.base import BaseModel
 from laktory.models.ingestionpattern import IngestionPattern
 
 
+FORMATS = (
+    "JSON",
+    "CSV",
+    "PARQUET",
+)
+
+
 class BaseReader(BaseModel):
+    fmt: Literal[FORMATS] = "JSON"
+    multiline: bool = False
 
     @abstractmethod
     def read(self, spark) -> DataFrame:
