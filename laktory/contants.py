6aadf4a6149630737ce838c3bf3e86f0c53222e2
osoucy <osoucy.transactions@gmail.com>
2023-07-24 23:55:27 -0400

    Update

--- laktory/contants.py
+++ laktory/contants.py
@@ -1,17 +1,43 @@
-import inspect
 from pyspark.sql import types
 
 
 SUPPORTED_TYPES = {}
-for k in vars(types):
-    o = getattr(types, k)
-    if "Type" in k and inspect.isclass(o):
-        try:
-            typ = o()
-        except TypeError:
-            continue
+for t in [
+    types.DataType(),
+    types.NullType(),
+    types.AtomicType(),
+    types.NumericType(),
+    types.IntegralType(),
+    types.FractionalType(),
+    types.StringType(),
+    types.BinaryType(),
+    types.BooleanType(),
+    types.DateType(),
+    types.TimestampType(),
+    types.TimestampNTZType(),
+    types.DecimalType(10, 0),
+    types.DoubleType(),
+    types.FloatType(),
+    types.ByteType(),
+    types.IntegerType(),
+    types.LongType(),
+    types.DayTimeIntervalType(0, 3),
+    types.ShortType(),
+    types.StructType([]),
+    types.UserDefinedType()
+]:
+    SUPPORTED_TYPES[t.simpleString()] = t
 
-        if not hasattr(typ, "simpleString"):
-            continue
 
-        SUPPORTED_TYPES[typ.simpleString()] = typ
+# for k in vars(types):
+#     o = getattr(types, k)
+#     if "Type" in k and inspect.isclass(o):
+#         try:
+#             typ = o()
+#         except TypeError:
+#             continue
+#
+#         if not hasattr(typ, "simpleString"):
+#             continue
+#
+#         SUPPORTED_TYPES[typ.simpleString()] = typ
