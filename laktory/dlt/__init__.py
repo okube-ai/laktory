5400b044c8c5175a9d29d2fb993f6df701933419
osoucy <osoucy.transactions@gmail.com>
2023-08-01 02:33:38 -0400

    Update

--- laktory/dlt/__init__.py
+++ laktory/dlt/__init__.py
@@ -1,7 +1,30 @@
 try:
+    # Import databricks dlt module, only available for DBR >= 13
     from dlt import *
 except (ModuleNotFoundError, FileNotFoundError):
     try:
+        # Import local copy for dlt for DBR < 13
         from laktory._databricks_dlt import *
     except RuntimeError:
+        # Fail if spark context not available
         pass
+
+
+# --------------------------------------------------------------------------- #
+# Utilities                                                                   #
+# --------------------------------------------------------------------------- #
+
+def is_pipeline():
+    try:
+        import dlt
+    except (ModuleNotFoundError, FileNotFoundError):
+        return False
+    dbr_version = spark.conf.get("pipelines.dbrVersion", None)
+    if dbr_version is None:
+        return False
+    return True
+
+
+# --------------------------------------------------------------------------- #
+# Overwrite                                                                   #
+# --------------------------------------------------------------------------- #
