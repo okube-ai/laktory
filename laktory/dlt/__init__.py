bc3c4671e70f1a4b9146086f8a37ff123161aa92
osoucy <osoucy.transactions@gmail.com>
2023-08-02 01:59:40 -0400

    Update

--- laktory/dlt/__init__.py
+++ laktory/dlt/__init__.py
@@ -1,6 +1,7 @@
 try:
     # Import databricks dlt module, only available for DBR >= 13
     from dlt import *
+    from dlt import read as _read
 except (ModuleNotFoundError, FileNotFoundError):
     try:
         # Import local copy for dlt for DBR < 13
@@ -28,3 +29,13 @@ def is_pipeline(spark):
 # --------------------------------------------------------------------------- #
 # Overwrite                                                                   #
 # --------------------------------------------------------------------------- #
+
+
+def read(*args, spark=None, **kwargs):
+    if is_pipeline(spark):
+        # Remove catalog and database from naming space
+        args = list(args)
+        args[0] = args[0].split(".")[-1]
+        return _read(*args, **kwargs)
+    else:
+        return spark.read.table(args[0])
