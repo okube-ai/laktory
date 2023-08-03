705a8eb69584ab06d98e5c8a804619fd9475d3e4
osoucy <osoucy.transactions@gmail.com>
2023-08-02 23:42:42 -0400

    Update

--- laktory/_settings.py
+++ laktory/_settings.py
@@ -4,5 +4,10 @@ from settus import BaseSettings
 class Settings(BaseSettings):
     landing_mount_path: str = "mnt/landing/"
 
+    # https://learn.microsoft.com/en-ca/azure/databricks/dev-tools/auth#general-host-token-and-account-id-environment-variables-and-fields
+    databricks_host: str = ""
+    databricks_token: str = ""
+    databricks_account_id: str = ""
+
 
 settings = Settings()
