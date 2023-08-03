a5206ca7d9a833f7873bfbfd145f0b04b5429f7a
osoucy <osoucy.transactions@gmail.com>
2023-08-03 19:19:33 -0400

    Update

---
+++ laktory/models/eventheader.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import Field
+from pydantic import field_validator
+from pydantic_core.core_schema import FieldValidationInfo
+
+from laktory._settings import settings
+from laktory.models.base import BaseModel
+from laktory.models.producer import Producer
+
+
+class EventHeader(BaseModel):
+    name: str
+    description: str = None
+    producer: Producer = None
+    landing_mount_path: str = settings.landing_mount_path
+    dirpath: Optional[str] = Field(validate_default=True, default=None)
+
+    # ----------------------------------------------------------------------- #
+    # Paths                                                                   #
+    # ----------------------------------------------------------------------- #
+
+    @field_validator("dirpath")
+    def default_dirpath(cls, v: str, info: FieldValidationInfo) -> str:
+        if v is None:
+            data = info.data
+            producer = ""
+            if data["producer"] is not None:
+                producer = data["producer"].name + "/"
+            v = f'{data["landing_mount_path"]}events/{producer}{data["name"]}/'
+        return v
--- tests/test_eventdata.py
+++ tests/test_eventdata.py
@@ -21,24 +21,27 @@ def test_eventdata():
     assert event.data["_producer_name"] == "FDR"
     assert event.data["_created_at"] == datetime(2023, 7, 1, 1, 0, 0, tzinfo=ZoneInfo("UTC"))
     assert event.created_at == datetime(2023, 7, 1, 1, 0, 0, tzinfo=ZoneInfo("UTC"))
-    assert event.get_landing_filepath() == "mnt/landing/events/FDR/flight_record/2023/07/01/flight_record_20230701T010000000Z.json"
+    assert event.get_filepath() == "mnt/landing/events/FDR/flight_record/2023/07/01/flight_record_20230701T010000000Z.json"
 
 
 def test_model_dump():
 
     d = event.model_dump()
+    print(d)
+    for k, v in d.items():
+        print(k, v)
     assert d == {
-        'name': 'flight_record',
-        'description': None,
-        'producer': {'name': 'FDR', 'description': None, 'party': 1},
-        'data': {
-            'created_at': '2023-07-01T01:00:00',
-            'airspeed': 100.0,
-            'altitude': 20000.0,
-            '_name': 'flight_record',
-            '_producer_name': 'FDR',
-            '_created_at': '2023-07-01T01:00:00Z'
-        }
+        "name": "flight_record",
+        "description": None,
+        "producer": {"name": "FDR", "description": None, "party": 1},
+        "dirpath": "mnt/landing/events/FDR/flight_record/",
+        "data": {
+            "created_at": "2023-07-01T01:00:00",
+            "airspeed": 100.0,
+            "altitude": 20000.0,
+            "_name": "flight_record",
+            "_producer_name": "FDR",
+            "_created_at": "2023-07-01T01:00:00Z"}
     }
 
 
