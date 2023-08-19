295404738a28ef2eb16c911bc15d954e3ea4a649
osoucy <osoucy.transactions@gmail.com>
2023-08-18 22:52:44 -0400

    Update

--- laktory/models/base.py
+++ laktory/models/base.py
@@ -1,12 +1,11 @@
 import yaml
 import json
 import jsonref
-import time
 from pydantic import BaseModel as _BaseModel
 from pydantic import ConfigDict
 
-from laktory import settings
 from laktory.sql import value_to_statement
+from laktory.databricks.workspaceclient import WorkspaceClient
 
 
 class BaseModel(_BaseModel):
@@ -14,44 +13,7 @@ class BaseModel(_BaseModel):
 
     @property
     def workspace_client(self):
-        from databricks.sdk import WorkspaceClient
-        return WorkspaceClient(
-            host=settings.databricks_host,
-            token=settings.databricks_token,
-        )
-
-    def execute_statement_and_wait(self, statement, warehouse_id=None, catalog_name=None):
-
-        from databricks.sdk.service.sql import StatementState
-
-        w = self.workspace_client
-
-        # Settings
-        if warehouse_id is None:
-            warehouse_id = settings.databricks_warehouse_id
-
-        if catalog_name is None:
-            catalog_name = getattr(self, "catalog_name", None)
-
-        r = w.statement_execution.execute_statement(
-            statement=statement,
-            catalog=catalog_name,
-            warehouse_id=warehouse_id,
-        )
-        statement_id = r.statement_id
-        state = r.status.state
-
-        while state in [StatementState.PENDING, StatementState.RUNNING]:
-            r = w.statement_execution.get_statement(statement_id)
-            time.sleep(1)
-            state = r.status.state
-
-        if state != StatementState.SUCCEEDED:
-            # TODO: Create specific error
-            print(r.status.error.message)
-            raise Exception(r.status.error)
-
-        return r
+        return WorkspaceClient()
 
     @classmethod
     def model_validate_yaml(cls, fp):
