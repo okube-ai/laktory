70210bd5f80f4bb1c2f327236d96c970f242ec31
osoucy <osoucy.transactions@gmail.com>
2023-08-22 02:49:18 -0400

    Update

--- laktory/models/sources/basesource.py
+++ laktory/models/sources/basesource.py
@@ -1,6 +1,12 @@
+from abc import abstractmethod
+from pyspark.sql import DataFrame
+
 from laktory.models.base import BaseModel
 
 
 class BaseSource(BaseModel):
     read_as_stream: bool | None = True
 
+    @abstractmethod
+    def read(self, spark) -> DataFrame:
+        raise NotImplementedError()
