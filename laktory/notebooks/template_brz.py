919a5ecb5bc40b416fa8dbda83adf2048d9ad141
osoucy <osoucy.transactions@gmail.com>
2023-08-24 03:30:32 -0400

    Update

--- laktory/notebooks/template_brz.py
+++ laktory/notebooks/template_brz.py
@@ -1,30 +1,34 @@
-
 import json
 import pyspark.sql.functions as F
 
 from laktory import dlt
 from laktory import models
 from laktory import settings
+from laktory._logger import get_logger
+
 dlt.spark = spark
+logger = get_logger(__name__)
 
 pl_name = "pl-stock-prices"
 
 
 # TODO: Add supports for DBR 12.2? Or Not?
 def define_bronze_table(table):
-
     @dlt.table(
         name=table.name,
         comment=table.comment,
-        #path=TODO,
+        # path=TODO,
     )
     def get_df():
+        logger.info(f"Building {table.name} table")
 
         # Read Source
         source = table.source
         df = source.read(spark)
 
         return df
+
+    return get_df
 
 
 tables = (
@@ -36,4 +40,9 @@ tables = (
 for row in tables.collect():
     d = row.asDict(recursive=True)
     d["columns"] = json.loads(d["columns"])
-    df = define_bronze_table(models.Table(**d))
+    table = models.Table(**d)
+    dataset = define_bronze_table(table)()
+
+    if not dlt.is_pipeline():
+        df = dataset.func()
+        display(df)
