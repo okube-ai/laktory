d066a18ac91932e3f62eaf27602748d66539f89f
olivier.soucy <olivier.soucy@taigamotors.ca>
2023-10-09 09:28:40 -0400

    Update

--- laktory/models/grants/schemagrant.py
+++ laktory/models/grants/schemagrant.py
@@ -17,6 +17,7 @@ PRIVILEGES = TABLE_PRIVILEGES + VOLUME_PRIVILEGES + FUNCTION_PRIVILEGES + MODEL_
     "USE_SCHEMA",
 ]
 
+
 class SchemaGrant(BaseModel):
     principal: str
     privileges: list[Literal[tuple(PRIVILEGES)]]
