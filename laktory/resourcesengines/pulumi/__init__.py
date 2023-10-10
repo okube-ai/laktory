6c3d8f50fc0edf7f9b4190948e73f157f5854cae
olivier.soucy <olivier.soucy@taigamotors.ca>
2023-10-09 21:56:48 -0400

    Update

--- laktory/resourcesengines/pulumi/__init__.py
+++ laktory/resourcesengines/pulumi/__init__.py
@@ -4,3 +4,4 @@ from laktory.resourcesengines.pulumi.schema import PulumiSchema
 from laktory.resourcesengines.pulumi.serviceprincipal import PulumiServicePrincipal
 from laktory.resourcesengines.pulumi.user import PulumiUser
 from laktory.resourcesengines.pulumi.volume import PulumiVolume
+from laktory.resourcesengines.pulumi.base import PulumiResourcesEngine
