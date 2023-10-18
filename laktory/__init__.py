cf9e3ad3ba050a81b822eb6fab9d9bbc33d3ac33
osoucy <osoucy.transactions@gmail.com>
2023-10-18 01:10:26 -0400

    Update

--- laktory/__init__.py
+++ laktory/__init__.py
@@ -19,4 +19,4 @@ from ._settings import Settings
 # Objects                                                                     #
 # --------------------------------------------------------------------------- #
 
-from metadata import read_metadata
+from .metadata import read_metadata
