54ccdea7246ca1f4ba5ce28d0b18853084a497df
osoucy <osoucy.transactions@gmail.com>
2023-10-20 03:04:35 -0400

    Update

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,14 +5,14 @@ from laktory import Settings
 
 def test_settings():
     settings0 = Settings()
-    settings1 = Settings(landing_mount_path="/mnt")
-    os.environ["LAKTORY_LANDING_MOUNT_PATH"] = "staging"
+    settings1 = Settings(workspace_landing_root="/mnt/landing/")
+    os.environ["LAKTORY_WORKSPACE_LANDING_ROOT"] = "staging"
     settings2 = Settings()
-    del os.environ["LAKTORY_LANDING_MOUNT_PATH"]
+    del os.environ["LAKTORY_WORKSPACE_LANDING_ROOT"]
 
-    assert settings0.landing_mount_path == "/mnt/landing/"
-    assert settings1.landing_mount_path == "/mnt"
-    assert settings2.landing_mount_path == "staging"
+    assert settings0.workspace_landing_root == "/Volumes/dev/sources/landing/"
+    assert settings1.workspace_landing_root == "/mnt/landing/"
+    assert settings2.workspace_landing_root == "staging"
 
 
 if __name__ == "__main__":
