1fb194b6c25f341a17bc1b1db204e4db7a3f4868
osoucy <osoucy.transactions@gmail.com>
2023-11-04 22:40:06 -0400

    Update

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,14 +1,9 @@
-import pytest
-from datetime import datetime
 from pydantic import ValidationError
+from pyspark.sql import types as T
 import pandas as pd
+import pytest
 
-from laktory.models import Catalog
-from laktory.models import Schema
-from laktory.models import Column
 from laktory.models import Table
-from laktory.models import EventDataSource
-from laktory.models import TableDataSource
 from laktory._testing import table_brz
 from laktory._testing import table_slv
 from laktory._testing import EventsManager
@@ -40,13 +35,30 @@ def test_model():
                 "pii": None,
                 "schema_name": "markets",
                 "spark_func_args": [
-                    {"value": "_created_at", "to_column": True, "to_lit": None}
+                    {"value": "_created_at", "to_column": True, "to_lit": None},
+                    {"value": "data._created_at", "to_column": True, "to_lit": None},
                 ],
                 "spark_func_kwargs": {},
                 "spark_func_name": "coalesce",
                 "sql_expression": None,
                 "table_name": "slv_stock_prices",
                 "type": "timestamp",
+                "unit": None,
+            },
+            {
+                "catalog_name": "dev",
+                "comment": None,
+                "name": "symbol",
+                "pii": None,
+                "schema_name": "markets",
+                "spark_func_args": [
+                    {"value": "data.symbol", "to_column": True, "to_lit": None}
+                ],
+                "spark_func_kwargs": {},
+                "spark_func_name": "coalesce",
+                "sql_expression": None,
+                "table_name": "slv_stock_prices",
+                "type": "string",
                 "unit": None,
             },
             {
@@ -113,13 +125,27 @@ def test_silver():
     df0 = manager.to_spark_df()
     df1 = table_brz.process_bronze(df0)
     df2 = table_slv.process_silver(df1)
-    df2.show()
-    # assert "_bronze_at" in df1.columns
+    assert df2.schema == T.StructType(
+        [
+            T.StructField("created_at", T.TimestampType(), True),
+            T.StructField("symbol", T.StringType(), True),
+            T.StructField("open", T.DoubleType(), True),
+            T.StructField("close", T.DoubleType(), True),
+            T.StructField("_silver_at", T.TimestampType(), False),
+        ]
+    )
+    s = df2.toPandas().iloc[0]
+    assert s.to_dict() == {
+        "created_at": pd.Timestamp("2023-08-31 20:00:00"),
+        "symbol": "AAPL",
+        "open": 189.49000549316406,
+        "close": 189.49000549316406,
+        "_silver_at": pd.Timestamp("2023-11-04 22:32:13.219578"),
+    }
 
 
 if __name__ == "__main__":
-    # test_model()
-    # test_data()
-    # test_bronze()
+    test_model()
+    test_data()
+    test_bronze()
     test_silver()
-
