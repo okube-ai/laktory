92408a6371b3558ac6c7b241baf344a450525e61
osoucy <osoucy.transactions@gmail.com>
2023-11-11 21:07:37 -0500

    Update

--- tests/test_datasources.py
+++ tests/test_datasources.py
@@ -4,11 +4,8 @@ import os
 import pytest
 
 from laktory.models.datasources import EventDataSource
-from laktory.models.datasources import TableJoinDataSource
 from laktory._testing import StockPriceDataEventHeader
 from laktory._testing import EventsManager
-from laktory._testing import table_slv
-from laktory._testing import df_meta
 
 
 # Build and write events
@@ -53,49 +50,6 @@ def test_event_data_source_read():
     assert row["close"] == pytest.approx(189.46, abs=0.01)
 
 
-def test_table_join_data_source():
-    source = TableJoinDataSource(
-        left={
-            "name": "slv_stock_prices",
-            "filter": "created_at = '2023-11-01T00:00:00Z'",
-        },
-        other={
-            "name": "slv_stock_metadata",
-        },
-        on=["symbol"],
-        columns=[
-            "symbol",
-            "currency",
-            "first_traded",
-        ],
-    )
-    source.left._df = table_slv.to_df(spark)
-    source.other._df = df_meta
-
-    df = source.read(spark)
-    data = df.toPandas().to_dict(orient="records")
-    print(data)
-    assert data == [
-        {
-            "created_at": "2023-11-01T00:00:00Z",
-            "symbol": "AAPL",
-            "open": 1,
-            "close": 2,
-            "currency": "USD",
-            "first_traded": "1980-12-12T14:30:00.000Z",
-        },
-        {
-            "created_at": "2023-11-01T00:00:00Z",
-            "symbol": "GOOGL",
-            "open": 3,
-            "close": 4,
-            "currency": "USD",
-            "first_traded": "2004-08-19T13:30:00.00Z",
-        },
-    ]
-
-
 if __name__ == "__main__":
     test_event_data_source()
     test_event_data_source_read()
-    test_table_join_data_source()
