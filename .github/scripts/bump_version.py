7ccffc5c961f7da11fc9f514114d8e7441b70f67
osoucy <osoucy.transactions@gmail.com>
2023-11-29 00:38:11 -0500

    Test release workflow

--- .github/scripts/bump_version.py
+++ .github/scripts/bump_version.py
@@ -1,7 +1,88 @@
 import os
+import logging
+from datetime import datetime
+from packaging import version
 
+script_root = os.path.dirname(__file__)
 laktory_root = "./laktory/"
 
-# Read version file
-with open(os.path.join(laktory_root, "_version.py")) as fp:
-    fp.read()
+
+def main():
+
+    # Filepaths
+    version_filepath = os.path.join(laktory_root, "_version.py")
+    git_env_filepath = os.getenv("GITHUB_OUTPUT", os.path.join(script_root, "git.env"))
+    changelog_filepath = os.path.join("./", "CHANGELOG.md")
+
+    # Read version file
+    with open(version_filepath) as fp:
+        v0 = fp.read().split("=")[-1].strip().replace('"', '')
+        v0 = version.parse(v0)
+        v1 = version.Version(f"{v0.major}.{v0.minor}.{v0.micro + 1}")
+    logging.info(f"Bumping laktory to {v1}")
+
+    # Update version file
+    logging.info(f"Updating _version.py")
+    with open(version_filepath, "w") as fp:
+        fp.write(f'VERSION = "{v1}"')
+
+    # Set version as git action variable
+    logging.info(f"Setting git env var version")
+    with open(git_env_filepath, 'a') as fp:
+        fp.write(f"version={str(v1)}")
+
+    # Update CHANGELOG
+    update_changelog(changelog_filepath, v0, v1)
+
+    # Cleanup
+    if os.path.exists(git_env_filepath):
+        os.remove(git_env_filepath)
+
+
+def update_changelog(changelog_filepath, v0, v1):
+
+    # Update CHANGELOG
+    logging.info(f"Updating changelog")
+    with open(changelog_filepath, 'r') as fp:
+        content = fp.read()
+
+    print(content)
+
+    # Set version line
+    version_line = f'## [{v0}] - Unreleased'
+
+    # Validate
+    if version_line not in content:
+        msg = f"CHANGELOG  does not include version {v0}"
+        logging.info('ValueError: ' + msg)
+        raise ValueError(msg)
+
+    # Update current version release date
+    today = datetime.utcnow()
+    new_version_line = f"## [{v0}] - {today.strftime('%Y-%m-%d')}"
+    content = content.replace(version_line, new_version_line)
+
+    # Add placeholder for next version
+    next_version_lines = [
+        '# Release History',
+        '',
+        f'## [{v1}] - Unreleased',
+        '###Added',
+        '* n/a',
+        '###Fixed',
+        '* n/a',
+        '###Updated',
+        '* n/a',
+        '###Breaking changes',
+        '* n/a',
+        ''
+    ]
+    content = content.replace('# Release History\n', '\n'.join(next_version_lines))
+
+    # Write
+    with open(changelog_filepath, 'w') as fp:
+        fp.write(content)
+
+
+if __name__ == "__main__":
+    main()
