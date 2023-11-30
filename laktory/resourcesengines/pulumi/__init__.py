e0e44e13b981c3f4ed34e544bda1206eee2fbfa9
osoucy <osoucy.transactions@gmail.com>
2023-11-30 01:43:25 -0500

    Update

--- laktory/resourcesengines/pulumi/__init__.py
+++ laktory/resourcesengines/pulumi/__init__.py
@@ -8,3 +8,4 @@ from laktory.resourcesengines.pulumi.schema import PulumiSchema
 from laktory.resourcesengines.pulumi.serviceprincipal import PulumiServicePrincipal
 from laktory.resourcesengines.pulumi.user import PulumiUser
 from laktory.resourcesengines.pulumi.volume import PulumiVolume
+from laktory.resourcesengines.pulumi.table import PulumiTable
---
+++ laktory/resourcesengines/pulumi/table.py
@@ -0,0 +1,51 @@
+import pulumi
+import pulumi_databricks as databricks
+from laktory.resourcesengines.pulumi.base import PulumiResourcesEngine
+from laktory.models.sql.table import Table
+
+from laktory._logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class PulumiTable(PulumiResourcesEngine):
+    @property
+    def provider(self):
+        return "databricks"
+
+    def __init__(
+        self,
+        name=None,
+        table: Table = None,
+        opts=None,
+    ):
+        if name is None:
+            name = f"table-{table.full_name}"
+        super().__init__(self.t, name, {}, opts)
+
+        opts = pulumi.ResourceOptions(
+            parent=self,
+        )
+
+        # Table
+        if table.builder.pipeline_name is None:
+            self.table = databricks.SqlTable(
+                f"table-{table.full_name}",
+                opts=opts,
+                **table.model_pulumi_dump(),
+            )
+
+        # Schema grants
+        _opts = opts.merge(pulumi.ResourceOptions(depends_on=self.table))
+        if table.grants:
+            self.grants = databricks.Grants(
+                f"grants-{table.full_name}",
+                table=table.full_name,
+                grants=[
+                    databricks.GrantsGrantArgs(
+                        principal=g.principal, privileges=g.privileges
+                    )
+                    for g in table.grants
+                ],
+                opts=_opts,
+            )
