61002c37d5c066943ebf16282306c6b822d383e4
osoucy <osoucy.transactions@gmail.com>
2023-12-08 16:07:03 -0500

    Update

---
+++ docs/api/databricks/warehouse.md
@@ -0,0 +1,9 @@
+::: laktory.models.Warehouse
+
+---
+
+::: laktory.models.databricks.warehouse.WarehouseTags
+
+---
+
+::: laktory.models.databricks.warehouse.WarehouseCustomTag
--- laktory/models/databricks/warehouse.py
+++ laktory/models/databricks/warehouse.py
@@ -5,15 +5,70 @@ from laktory.models.databricks.permission import Permission
 
 
 class WarehouseCustomTag(BaseModel):
+    """
+    Warehouse Custom Tag specifications
+
+    Attributes
+    ----------
+    key:
+        Tag key
+    value:
+        Tag value
+    """
     key: str
     value: str
 
 
 class WarehouseTags(BaseModel):
+    """
+    Warehouse Tags specifications
+
+    Attributes
+    ----------
+    custom_tags:
+        Tags specifications
+    """
     custom_tags: list[WarehouseCustomTag] = []
 
 
 class Warehouse(BaseModel, BaseResource):
+    """
+    Databricks Warehouse
+
+    Attributes
+    ----------
+    auto_stop_mins:
+        Time in minutes until an idle SQL warehouse terminates all clusters and stops.
+    channel_name:
+        Channel specifications
+    cluster_size:
+        The size of the clusters allocated to the endpoint
+    enable_photon:
+        If `True`, photon is enabled
+    enable_serverless_compute:
+        If `True`, warehouse is serverless.
+        See details [here](https://www.pulumi.com/registry/packages/databricks/api-docs/sqlendpoint/)
+    instance_profile_arn:
+        TODO
+    jdbc_url:
+        JDBC connection string.
+    max_num_clusters:
+        Maximum number of clusters available when a SQL warehouse is running.
+    min_num_clusters:
+        Minimum number of clusters available when a SQL warehouse is running.
+    name:
+        Warehouse name
+    num_clusters:
+        Fixed number of clusters when autoscaling is not enabled.
+    permissions:
+        Warehouse permissions
+    spot_instance_policy:
+        The spot policy to use for allocating instances to clusters.
+    tags:
+        Databricks tags all endpoint resources with these tags.
+    warehouse_type:
+        SQL warehouse type.
+    """
     cluster_size: Literal[
         "2X-Small",
         "X-Small",
@@ -37,13 +92,11 @@ class Warehouse(BaseModel, BaseResource):
     name: str = None
     num_clusters: int = None
     # odbc_params
+    permissions: list[Permission] = []
     spot_instance_policy: Literal["COST_OPTIMIZED", "RELIABILITY_OPTIMIZED"] = None
     # state
     tags: WarehouseTags = None
     warehouse_type: Literal["CLASSIC", "PRO"] = None
-
-    # Deployment Options
-    permissions: list[Permission] = []
 
     # ----------------------------------------------------------------------- #
     # Resources Engine Methods                                                #
@@ -58,7 +111,22 @@ class Warehouse(BaseModel, BaseResource):
         d["channel"] = {"name": d.pop("channel_name")}
         return d
 
-    def deploy_with_pulumi(self, name=None, groups=None, opts=None):
+    def deploy_with_pulumi(self, name=None, opts=None):
+        """
+        Deploy warehouse using pulumi.
+
+        Parameters
+        ----------
+        name:
+            Name of the pulumi resource. Default is `warehouse-{self.name}`
+        opts:
+            Pulumi resource options
+
+        Returns
+        -------
+        PulumiWarehouse:
+            Pulumi warehouse resource
+        """
         from laktory.resourcesengines.pulumi.warehouse import PulumiWarehouse
 
         return PulumiWarehouse(name=name, warehouse=self, opts=opts)
