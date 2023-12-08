db48651e9d71e37f25341fbd97b593e5417466c7
osoucy <osoucy.transactions@gmail.com>
2023-12-08 15:51:29 -0500

    Update

--- laktory/models/databricks/notebook.py
+++ laktory/models/databricks/notebook.py
@@ -8,6 +8,21 @@ from laktory.models.databricks.permission import Permission
 
 
 class Notebook(BaseModel, BaseResource):
+    """
+    Databricks Notebook
+
+    Attributes
+    ----------
+    source:
+        Path to notebook in source code format on local filesystem.
+    dirpath:
+        Workspace directory containing the notebook. Filename will be assumed
+        to be the same as local filepath.
+    language:
+         Notebook programming language
+    permissions:
+        List of notebook permissions
+    """
     source: str
     dirpath: str = None
     path: str = None
@@ -15,11 +30,13 @@ class Notebook(BaseModel, BaseResource):
     permissions: list[Permission] = []
 
     @property
-    def filename(self):
+    def filename(self) -> str:
+        """Notebook file name"""
         return os.path.basename(self.source)
 
     @property
-    def key(self):
+    def key(self) -> str:
+        """Notebook resource key"""
         key = os.path.splitext(self.path)[0].replace("/", "-")
         if key.startswith("-"):
             key = key[1:]
@@ -45,6 +62,21 @@ class Notebook(BaseModel, BaseResource):
         return ["permissions", "dirpath"]
 
     def deploy_with_pulumi(self, name=None, groups=None, opts=None):
+        """
+        Deploy notebook using pulumi.
+
+        Parameters
+        ----------
+        name:
+            Name of the pulumi resource. Default is `notebook-{self.key}`
+        opts:
+            Pulumi resource options
+
+        Returns
+        -------
+        PulumiNotebook:
+            Pulumi notebook resource
+        """
         from laktory.resourcesengines.pulumi.notebook import PulumiNotebook
 
         return PulumiNotebook(name=name, notebook=self, opts=opts)
