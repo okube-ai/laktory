40174590612a4fdf038d8dc14e31d35271a82c56
olivier.soucy <olivier.soucy@taigamotors.ca>
2023-12-10 13:46:53 -0500

    Update

--- laktory/spark/dataframe/has_column.py
+++ laktory/spark/dataframe/has_column.py
@@ -1,7 +1,7 @@
 import re
 from pyspark.sql.dataframe import DataFrame
 
-from laktory.spark.dataframe.flat_schema import schema_flat
+from laktory.spark.dataframe.schema_flat import schema_flat
 
 
 def has_column(df: DataFrame, col: str) -> bool:
@@ -19,7 +19,129 @@ def has_column(df: DataFrame, col: str) -> bool:
     -------
     :
         Result
+
+    Examples
+    --------
+
+    ```py
+    import laktory
+    from pyspark.sql import SparkSession
+    import pyspark.sql.types as T
+    spark = SparkSession.builder.getOrCreate()
+
+    schema = T.StructType(
+        [
+            T.StructField("indexx", T.IntegerType()),
+            T.StructField(
+                "stock",
+                T.StructType(
+                    [
+                        T.StructField("symbol", T.StringType()),
+                        T.StructField("name", T.StringType()),
+                    ]
+                ),
+            ),
+            T.StructField(
+                "prices",
+                T.ArrayType(
+                    T.StructType(
+                        [
+                            T.StructField("open", T.IntegerType()),
+                            T.StructField("close", T.IntegerType()),
+                        ]
+                    )
+                ),
+            ),
+        ]
+    )
+
+    data = [
+        (
+            1,
+            {"symbol": "AAPL", "name": "Apple"},
+            [{"open": 1, "close": 2}, {"open": 1, "close": 2}],
+        ),
+        (
+            2,
+            {"symbol": "MSFT", "name": "Microsoft"},
+            [{"open": 1, "close": 2}, {"open": 1, "close": 2}],
+        ),
+        (
+            3,
+            {"symbol": "GOOGL", "name": "Google"},
+            [{"open": 1, "close": 2}, {"open": 1, "close": 2}],
+        ),
+    ]
+
+    df = spark.createDataFrame(data, schema=schema)
+    print(df.has_column("symbol"))
+    #> False
+    print(df.has_column("`stock`.`symbol`"))
+    # > True
+    print(df.has_column("`prices[2]`.`close`"))
+    # > True
+    ```
     """
     _col = re.sub(r"\[(\d+)\]", r"[*]", col)
     _col = re.sub(r"`", "", _col)
     return _col in schema_flat(df)
+
+
+if __name__ == "__main__":
+
+    import laktory
+    from pyspark.sql import SparkSession
+    import pyspark.sql.types as T
+    spark = SparkSession.builder.getOrCreate()
+
+    schema = T.StructType(
+        [
+            T.StructField("indexx", T.IntegerType()),
+            T.StructField(
+                "stock",
+                T.StructType(
+                    [
+                        T.StructField("symbol", T.StringType()),
+                        T.StructField("name", T.StringType()),
+                    ]
+                ),
+            ),
+            T.StructField(
+                "prices",
+                T.ArrayType(
+                    T.StructType(
+                        [
+                            T.StructField("open", T.IntegerType()),
+                            T.StructField("close", T.IntegerType()),
+                        ]
+                    )
+                ),
+            ),
+        ]
+    )
+
+    data = [
+        (
+            1,
+            {"symbol": "AAPL", "name": "Apple"},
+            [{"open": 1, "close": 2}, {"open": 1, "close": 2}],
+        ),
+        (
+            2,
+            {"symbol": "MSFT", "name": "Microsoft"},
+            [{"open": 1, "close": 2}, {"open": 1, "close": 2}],
+        ),
+        (
+            3,
+            {"symbol": "GOOGL", "name": "Google"},
+            [{"open": 1, "close": 2}, {"open": 1, "close": 2}],
+        ),
+    ]
+
+    df = spark.createDataFrame(data, schema=schema)
+    print(df.has_column("symbol"))
+    #> False
+    print(df.has_column("`stock`.`symbol`"))
+    # > True
+    print(df.has_column("`prices[2]`.`close`"))
+    # > True
