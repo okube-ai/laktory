69814c138f9950b39edcf9fc0e2d7397b51f912c
osoucy <osoucy.transactions@gmail.com>
2023-12-11 02:20:22 -0500

    Update

---
+++ laktory/spark/functions/math2.py
@@ -0,0 +1,26 @@
+def x2(x):
+    """
+    This is a function.
+
+    Parameters
+    ----------
+    x: float
+        Input value
+
+    Returns
+    -------
+    output: float
+        Output value
+
+    Examples
+    --------
+    ```py
+    from laktory import x2
+
+    x2(2.0)
+    #> 5.0
+    ```
+    """
+
+    return x*2
+
