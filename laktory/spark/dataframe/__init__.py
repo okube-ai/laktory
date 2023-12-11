43e6b52c5c099e027d468b5f71338cce9eb74769
osoucy <osoucy.transactions@gmail.com>
2023-12-11 09:50:10 -0500

    Update

--- laktory/spark/dataframe/__init__.py
+++ laktory/spark/dataframe/__init__.py
@@ -2,11 +2,13 @@ from pyspark.sql.dataframe import DataFrame
 
 from laktory.spark.dataframe.schema_flat import schema_flat
 from laktory.spark.dataframe.has_column import has_column
+from laktory.spark.dataframe.show_string import show_string
 
 
 # DataFrame Extensions
 DataFrame.schema_flat = schema_flat
 DataFrame.has_column = has_column
+DataFrame.show_string = show_string
 
 # Spark Connect DataFrame Extensions
 try:
@@ -14,5 +16,6 @@ try:
 
     DataFrame.schema_flat = schema_flat
     DataFrame.has_column = has_column
+    DataFrame.show_string = show_string
 except:
     pass
