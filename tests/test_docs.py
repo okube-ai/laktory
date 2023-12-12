6b796afb982e777256bbfc075e7f3a4413f33860
osoucy <osoucy.transactions@gmail.com>
2023-12-11 20:54:19 -0500

    Update

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -5,26 +5,6 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 
-@pytest.mark.parametrize("example", find_examples("./laktory/spark/dataframe"), ids=str)
-def test_docstrings_spark_dataframe(example: CodeExample, eval_example: EvalExample):
-    if eval_example.update_examples:
-        eval_example.format(example)
-        eval_example.run_print_update(example, module_globals={"spark": spark})
-    else:
-        eval_example.lint(example)
-        eval_example.run_print_check(example, module_globals={"spark": spark})
-
-
-@pytest.mark.parametrize("example", find_examples("./laktory/spark/functions"), ids=str)
-def test_docstrings_spark_functions(example: CodeExample, eval_example: EvalExample):
-    if eval_example.update_examples:
-        eval_example.format(example)
-        eval_example.run_print_update(example, module_globals={"spark": spark})
-    else:
-        eval_example.lint(example)
-        eval_example.run_print_check(example, module_globals={"spark": spark})
-
-
 @pytest.mark.parametrize("example", find_examples("./laktory/dlt"), ids=str)
 def test_docstrings_spark_functions(example: CodeExample, eval_example: EvalExample):
     if eval_example.update_examples:
@@ -45,3 +25,39 @@ def test_docstrings_spark_functions(example: CodeExample, eval_example: EvalExam
                 "display": lambda x: x,
             },
         )
+
+
+@pytest.mark.parametrize("example", find_examples("./laktory/models/sql"), ids=str)
+def test_docstrings_spark_functions(example: CodeExample, eval_example: EvalExample):
+    if eval_example.update_examples:
+        eval_example.format(example)
+        eval_example.run_print_update(
+            example,
+            module_globals={"spark": spark},
+        )
+    else:
+        eval_example.lint(example)
+        eval_example.run_print_check(
+            example,
+            module_globals={"spark": spark})
+
+
+@pytest.mark.parametrize("example", find_examples("./laktory/spark/dataframe"), ids=str)
+def test_docstrings_spark_dataframe(example: CodeExample, eval_example: EvalExample):
+    if eval_example.update_examples:
+        eval_example.format(example)
+        eval_example.run_print_update(example, module_globals={"spark": spark})
+    else:
+        eval_example.lint(example)
+        eval_example.run_print_check(example, module_globals={"spark": spark})
+
+
+@pytest.mark.parametrize("example", find_examples("./laktory/spark/functions"), ids=str)
+def test_docstrings_spark_functions(example: CodeExample, eval_example: EvalExample):
+    if eval_example.update_examples:
+        eval_example.format(example)
+        eval_example.run_print_update(example, module_globals={"spark": spark})
+    else:
+        eval_example.lint(example)
+        eval_example.run_print_check(example, module_globals={"spark": spark})
+
