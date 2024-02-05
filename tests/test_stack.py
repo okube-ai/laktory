d0ad19aafa8c0882a30bf14763084bb95b1afbae
osoucy <osoucy.transactions@gmail.com>
2024-02-04 22:31:24 -0500

    Update

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -646,9 +646,9 @@ def test_all_resources():
 
 
 if __name__ == "__main__":
-    # test_stack_model()
-    # test_pulumi_stack()
-    # test_pulumi_preview()
-    # test_terraform_stack()
-    # test_terraform_plan()
+    test_stack_model()
+    test_pulumi_stack()
+    test_pulumi_preview()
+    test_terraform_stack()
+    test_terraform_plan()
     test_all_resources()
