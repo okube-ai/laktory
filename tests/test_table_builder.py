ec012915db5a8d94f575c26ef558ab218e6e3d8a
osoucy <osoucy.transactions@gmail.com>
2024-04-29 21:47:11 -0400

    Update

--- tests/test_table_builder.py
+++ tests/test_table_builder.py
@@ -1,3 +1,4 @@
+import os
 from pyspark.sql import functions as F
 from pyspark.sql import SparkSession
 
@@ -6,9 +7,9 @@ from laktory import models
 # Spark
 spark = SparkSession.builder.appName("UnitTesting").getOrCreate()
 spark.conf.set("spark.sql.session.timeZone", "UTC")
-
-df_brz = spark.read.parquet("./data/brz_stock_prices")
-df_slv = spark.read.parquet("./data/slv_stock_prices")
+dirpath = os.path.dirname(__file__)
+df_brz = spark.read.parquet(os.path.join(dirpath, "./data/brz_stock_prices"))
+df_slv = spark.read.parquet(os.path.join(dirpath, "./data/slv_stock_prices"))
 
 
 def test_read_and_process():
