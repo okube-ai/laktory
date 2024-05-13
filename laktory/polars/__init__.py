d3c6fc8a8934725ad931195a16e8b978cfca3223
osoucy <osoucy.transactions@gmail.com>
2024-05-13 13:18:48 -0400

    Update

---
+++ laktory/polars/__init__.py
@@ -0,0 +1,13 @@
+try:
+    import polars
+
+    from polars import DataFrame as PolarsDataFrame
+
+    def is_polars_dataframe(df):
+        return isinstance(df, PolarsDataFrame)
+
+except ModuleNotFoundError:
+    # Mocks when polars is not installed
+
+    class PolarsDataFrame:
+        pass
--- laktory/spark/__init__.py
+++ laktory/spark/__init__.py
@@ -1,18 +1,32 @@
 try:
     import pyspark
 
-    from pyspark.sql.dataframe import DataFrame
-    from pyspark.sql.column import Column
+    from pyspark.sql.connect.dataframe import DataFrame as SparkConnectDataFrame
+    from pyspark.sql.dataframe import DataFrame as SparkDataFrame
+    from pyspark.sql.column import Column as SparkColumn
 
     import laktory.spark.dataframe
     import laktory.spark.functions
 
+    def is_spark_dataframe(df):
+        """Check if dataframe is Spark DataFrame or Spark Connect DataFrame"""
+
+        if isinstance(df, SparkConnectDataFrame):
+            return True
+
+        if isinstance(df, SparkDataFrame):
+            return True
+
+        return False
 
 except ModuleNotFoundError:
     # Mocks when pyspark is not installed
 
-    class DataFrame:
+    class SparkDataFrame:
         pass
 
-    class Column:
+    class SparkConnectDataFrame:
+        pass
+
+    class SparkColumn:
         pass
