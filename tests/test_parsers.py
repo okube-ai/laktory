9460722f3d8cc65fd7e2fd103837f82f7b9bac7e
osoucy <osoucy.transactions@gmail.com>
2024-05-29 22:30:44 -0400

    Improved nodes data source reading in DLT debugging mode.

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,6 +1,7 @@
 import laktory
 from laktory.models import BaseModel
 from laktory._parsers import remove_empty
+from laktory._parsers import merge_dicts
 
 
 class Camel(BaseModel):
@@ -32,6 +33,58 @@ def test_remove_empty():
     assert remove_empty(d) == {"a": "a", "c": ["c"]}
 
 
+def test_merge_dicts():
+
+    baseline = {
+        "name": "Apple",
+        "symbols": {
+            "nasdaq": "AAPL",
+            "sp500": "APPL",
+        },
+        "prices": [
+            {
+                "timestamp": 0,
+                "open": 1,
+                "close": 2,
+            },
+            {
+                "timestamp": 1,
+                "open": 3,
+                "close": 4,
+            },
+            {
+                "timestamp": 2,
+                "open": 5,
+                "close": 6,
+            },
+        ],
+    }
+
+    overwrite = {
+        "symbols": {
+            "sp500": "AAPL",
+        },
+        "prices": {
+            1: {
+                "close": 5,
+            }
+        },
+    }
+
+    result = merge_dicts(baseline, overwrite)
+
+    assert result == {
+        "name": "Apple",
+        "symbols": {"nasdaq": "AAPL", "sp500": "AAPL"},
+        "prices": [
+            {"timestamp": 0, "open": 1, "close": 2},
+            {"timestamp": 1, "open": 3, "close": 5},
+            {"timestamp": 2, "open": 5, "close": 6},
+        ],
+    }
+
+
 if __name__ == "__main__":
-    test_camel_case()
-    test_remove_empty()
+    # test_camel_case()
+    # test_remove_empty()
+    test_merge_dicts()
