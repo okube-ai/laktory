9ddf95c9bd3cb0854c926e55be49619cf6d3c630
osoucy <osoucy.transactions@gmail.com>
2024-06-27 00:07:08 -0400

    Update

--- laktory/spark/session.py
+++ laktory/spark/session.py
@@ -1,21 +1,28 @@
+import inspect
+import importlib
 from functools import wraps
+import pyspark.sql.session
 from pyspark.sql.session import SparkSession
 
-_sql = SparkSession.sql
+filepath = inspect.getabsfile(pyspark.sql.session)
+
+spec = importlib.util.spec_from_file_location("spark0", filepath)
+spark0 = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(spark0)
 
 
 class LaktorySparkSession:
     """
     Databricks DLT module overwrite the spark.sql method, preventing from using
-    the latest parametrized queries. To circumvent this, we copy the baseline
-    sql function into a laktory namespace so that it can be used in addition to
-    the overwritten sql.
+    the latest parametrized queries. To circumvent this, we read the function
+    definition from the disk and assign it to a laktory namespace so that it
+    can be used in addition to the overwritten sql.
     """
     def __init__(self, spark: SparkSession):
         self._spark = spark
 
-    @wraps(SparkSession.sql)
+    @wraps(spark0.SparkSession.sql)
     def sql(self, *args, **kwargs):
-        return _sql(self._spark, *args, **kwargs)
+        return spark0.SparkSession.sql(self._spark, *args, **kwargs)
 
 SparkSession.laktory: LaktorySparkSession = property(lambda self: LaktorySparkSession(self))
