877692e84ac558df622911a6421369a8e1887837
osoucy <osoucy.transactions@gmail.com>
2024-07-28 02:15:46 -0400

    Update

--- laktory/models/datasinks/tabledatasink.py
+++ laktory/models/datasinks/tabledatasink.py
@@ -164,8 +164,9 @@ class TableDataSink(BaseDataSink):
         Delete sink data and checkpoints
         """
         if self._checkpoint_location:
-            logger.info(f"Deleting checkpoint at {self._checkpoint_location}")
-            if os.path.exists(self._checkpoint_location):
+            _exists = os.path.exists(self._checkpoint_location)
+            logger.info(f"Deleting checkpoint at {self._checkpoint_location}? {_exists}", )
+            if _exists:
                 shutil.rmtree(self._checkpoint_location)
 
         if self.warehouse == "DATABRICKS":
