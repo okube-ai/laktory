cb9a57f2354b3d3986932c59bc605b641206806f
osoucy <osoucy.transactions@gmail.com>
2025-01-06 02:35:25 -0500

    Update

--- laktory/__init__.py
+++ laktory/__init__.py
@@ -2,34 +2,18 @@ from ._version import VERSION
 
 __version__ = VERSION
 
-# Need to be imported first
+# Import first
 from ._settings import settings
-
-# --------------------------------------------------------------------------- #
-# User-agent                                                                  #
-# --------------------------------------------------------------------------- #
 from ._useragent import set_databricks_sdk_upstream
 
 set_databricks_sdk_upstream()
-
-
-# --------------------------------------------------------------------------- #
-# Packages                                                                    #
-# --------------------------------------------------------------------------- #
 
 import laktory._parsers
 import laktory.models
 import laktory.spark
 import laktory.types
 
-# --------------------------------------------------------------------------- #
-# Objects                                                                     #
-# --------------------------------------------------------------------------- #
 from ._logger import get_logger
-
-# --------------------------------------------------------------------------- #
-# Classes                                                                     #
-# --------------------------------------------------------------------------- #
 from ._settings import Settings
 from .cli.app import app
 from .datetime import unix_timestamp
