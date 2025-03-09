f7cf6fbb78c98509fb3d785c22d05b55392318e5
goda <480738+goda@users.noreply.github.com>
2025-03-09 13:13:39 +0000

    Update Grants resource references to use IDs instead of names in Databricks Table and Volume models

--- laktory/models/resources/databricks/table.py
+++ laktory/models/resources/databricks/table.py
@@ -199,7 +199,7 @@ class Table(BaseModel, PulumiResource, TerraformResource):
             resources += [
                 Grants(
                     resource_name=f"grants-{self.resource_name}",
-                    table=f"${{resources.{self.resource_name}.name}}",
+                    table=f"${{resources.{self.resource_name}.id}}",
                     grants=[
                         {"principal": g.principal, "privileges": g.privileges}
                         for g in self.grants
