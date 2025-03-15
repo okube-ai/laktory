e8b0dfad4b99197eeae107016db478500466637c
goda <480738+goda@users.noreply.github.com>
2025-03-15 14:22:08 +0000

    Adding validation to `UserLookup` to ensure it's instantiated correctly

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,7 @@
+from pydantic import ValidationError
 from laktory.models.resources.databricks import Group
 from laktory.models.resources.databricks import User
+from laktory.models.resources.databricks.user import UserLookup
 
 user = User(
     user_name="data.engineer@gmail.moc",
@@ -18,6 +20,22 @@ def test_user_group():
     assert user.roles == ["store-admin"]
     assert group.display_name == "role-engineers"
 
+def test_both_user_id_and_user_name():
+    try:
+        UserLookup(user_id=123, user_name="test_user")
+    except ValidationError:
+        pass
+    else:
+        assert False, "ValidationError not raised"
+
+def test_neither_user_id_and_user_name():
+    try:
+        UserLookup()
+    except ValidationError:
+        pass
+    else:
+        assert False, "ValidationError not raised"        
 
 if __name__ == "__main__":
     test_user_group()
+    test_both_user_id_and_user_name()
