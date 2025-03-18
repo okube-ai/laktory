2b0e40154f6f0fa4015784c8e35352bdf923eabc
goda <480738+goda@users.noreply.github.com>
2025-03-18 12:11:31 +0000

    Added group unit test check for additional_core_resources for

--- tests/test_group.py
+++ tests/test_group.py
@@ -1,4 +1,5 @@
 from laktory.models.resources.databricks import Group
+from laktory.models.resources.databricks.groupmember import GroupMember
 
 group = Group(
     display_name="role-engineers",
@@ -20,6 +21,11 @@ def test_group_members():
         "${resources.user-3.id}",
         "${resources.user-4.id}",
     ]
+
+    assert len(group.additional_core_resources) == 4
+    for r in group.additional_core_resources:
+        assert isinstance(r, GroupMember)
+        assert r.resource_key == f"{r.group_id}-{r.member_id}"
 
 
 if __name__ == "__main__":
