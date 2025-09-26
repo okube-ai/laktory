db0376f6f2bf5fd42795785106f776e5971319b5
osoucy <osoucy.transactions@gmail.com>
2025-09-26 11:28:29 -0400

    Update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,69 @@
+import os
+
 import narwhals as nw
 import pytest
+from databricks.sdk.core import Config
 
 from laktory import get_spark_session
+
+# --------------------------------------------------------------------------- #
+# Databricks                                                                  #
+# --------------------------------------------------------------------------- #
+
+
+def get_databricks_config() -> Config:
+    for profile_name in [
+        "laktory-dev-sp",
+        "laktory-dev-pat",
+    ]:
+        try:
+            return Config(profile=profile_name)
+        except ValueError:
+            pass
+
+    host = "https://adb-2211091707396001.1.azuredatabricks.net/"
+    token = os.getenv("DATABRICKS_TOKEN")
+    secret = os.getenv("AZURE_CLIENT_SECRET")
+
+    if token:
+        return Config(
+            host=host,
+            auth_type="pat",
+            serverless_compute_id="auto",
+        )
+
+    if secret:
+        return Config(
+            host=host,
+            auth_type="azure-client-secret",
+            azure_tenant_id=os.getenv("AZURE_TENANT_ID"),
+            azure_client_id=os.getenv("AZURE_CLIENT_ID"),
+            azure_client_secret=os.getenv("AZURE_CLIENT_SECRET"),
+            client_secret=secret,
+            serverless_compute_id="auto",
+        )
+
+    raise ValueError(
+        f"Databricks credentials could not be found. Setup profile '{profile_name}' or set DATABRICKS_TOKEN or AZURE_CLIENT_SECRET environment variables."
+    )
+
+
+def skip_dbks_test():
+    try:
+        get_databricks_config()
+    except ValueError as e:
+        pytest.skip(
+            f"Can't instantiate Databricks Config ({e}) not supported. Skipping Test."
+        )
+        return True
+
+
+@pytest.fixture()
+def wsclient():
+    from databricks.sdk import WorkspaceClient
+
+    return WorkspaceClient(config=get_databricks_config())
+
 
 # --------------------------------------------------------------------------- #
 # DataFrame                                                                   #
